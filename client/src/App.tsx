67664f6cfdde2a739cc19a1bb3cb5ac140214a8e
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2025-06-11 16:08:53 -0600

    feat: simplify App component by removing API calls and related state management for cleaner initialization flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,6 @@
-import { useState } from 'react'
 import beaver from './assets/beaver.svg'
-import { Button } from './components/ui/button'
-import { hcWithType } from 'server'
-
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3000"
-
-const client = hcWithType(SERVER_URL);
-
-type ResponseType = Awaited<ReturnType<typeof client.test.$get>>;
 
 function App() {
-  const [data, setData] = useState<Awaited<ReturnType<ResponseType["json"]>> | undefined>()
-
-  async function sendRequest() {
-    try {
-      const res = await client.test.$get()
-      if (!res.ok) {
-        console.log("Error fetching data")
-        return
-      }
-      const data = await res.json()
-      setData(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
     <div className="max-w-xl mx-auto flex flex-col gap-6 items-center justify-center min-h-screen">
       <a href="https://github.com/stevedylandev/bhvr" target="_blank">
@@ -38,21 +13,6 @@ function App() {
       <h1 className="text-5xl font-black">bhvr</h1>
       <h2 className="text-2xl font-bold">Bun + Hono + Vite + React</h2>
       <p>A typesafe fullstack monorepo</p>
-      <div className='flex items-center gap-4'>
-        <Button
-          onClick={sendRequest}
-        >
-          Call API
-        </Button>
-      </div>
-        {data && (
-          <pre className="bg-gray-100 p-4 rounded-md">
-            <code>
-            Message: {data.message} <br />
-            Success: {data.success.toString()}
-            </code>
-          </pre>
-        )}
     </div>
   )
 }
