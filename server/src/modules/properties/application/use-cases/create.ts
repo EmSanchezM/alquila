f1ee27d9d4a5a443d8547335c6093b9ff8aed805
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2025-06-13 14:16:05 -0600

    feat: enhance CreatePropertyUseCase to auto-generate IDs using generateUuid in property creation flow

--- server/src/modules/properties/application/use-cases/create.ts
+++ server/src/modules/properties/application/use-cases/create.ts
@@ -1,11 +1,15 @@
 import type { Property } from "@server/database/schema";
 import type { PropertiesRepository } from "@server/modules/properties/domain/repositories/properties.repository";
+import { generateUuid } from "@server/shared/generate-uuid";
 
 export class CreatePropertyUseCase {
   constructor(private readonly repository: PropertiesRepository) {}
-
-  async execute(payload: Omit<Property, 'id' | 'createdAt' | 'updatedAt'>): Promise<Property> {
-    const createdProperty = await this.repository.create(payload);
+  async execute(payload: Omit<Property, 'createdAt' | 'updatedAt'>): Promise<Property> {
+    const data = {
+      ...payload,
+      id: generateUuid(),
+    }
+    const createdProperty = await this.repository.create(data);
 
     if (!createdProperty) throw new Error('Property not created');
 
